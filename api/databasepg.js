340944cd3925ac056e4f7c68efe3709c57b21912
Miguel Werneck <mig.roale@gmail.com>
2023-12-23 12:22:40 -0300

    1

--- api/databasepg.js
+++ api/databasepg.js
@@ -10,35 +10,3 @@ const client = new Client({
 })
 
 module.exports = client;
-/*
-let notes = [];
-// Connect to the PostgreSQL database
-async function connectToDB(notes) {
-  try {
-    await client.connect(); // Connect to the database
-
-    // Query the database using 'async/await'
-    const result = await client.query('SELECT * FROM notes');
-
-    // Extract 'text' values from the result
-    notes.push(...result.rows.map(row => row.text)); // Using 'push' to add elements to 'notes'
-    console.log('Notes retrieved:', notes);
-  } catch (error) {
-    console.error(`Error connecting to the database: ${error.message}`);
-  } finally {
-    await client.end(); // Close the database connection
-  }
-}
-
-async function handleDatabase(notes) {
-  try {
-    await connectToDB(notes); // Wait for connectToDB() to finish retrieving notes
-    // Log 'notes' after 'connectToDB()' completes
-    console.log('Notes in list:', notes);
-  } catch (error) {
-    console.error('Error handling database:', error.message);
-  }
-}
-
-handleDatabase(notes);
-*/
